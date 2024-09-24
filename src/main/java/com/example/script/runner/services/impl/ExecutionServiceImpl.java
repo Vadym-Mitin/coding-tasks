10d7a64eea7ff487c1dbcbd93a056c36d6fb0ba7
Vadym-mitin <gpnt84@gmail.com>
2024-09-25 02:08:36 +0300

    fix warnings and tests

--- src/main/java/com/example/script/runner/services/impl/ExecutionServiceImpl.java
+++ src/main/java/com/example/script/runner/services/impl/ExecutionServiceImpl.java
@@ -1,9 +1,11 @@
 package com.example.script.runner.services.impl;
 
 import com.example.script.runner.services.ExecutionService;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class ExecutionServiceImpl implements ExecutionService {
     @Override
     public void execute(List<Integer> scripts) {
--- src/main/java/com/example/script/runner/services/impl/MockScriptRepository.java
+++ src/main/java/com/example/script/runner/services/impl/MockScriptRepository.java
@@ -2,9 +2,11 @@ package com.example.script.runner.services.impl;
 
 import com.example.script.runner.model.VulnerabilityScript;
 import com.example.script.runner.services.ScriptRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class MockScriptRepository implements ScriptRepository  {
     @Override
     public List<VulnerabilityScript> requestScripts() {
